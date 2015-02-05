bf037dcbac8f646025965b3788b03bb594c45d0f
tlclt <jordan.chayer@gmail.com>
2015-02-05 01:32:57 +0000

    blehhh

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-#def foo():
-    
-print('blabla')
+f = open('A-small-practice.in', 'r')
+T = int(f.readline()) #number of case
+N = 0
+L = 0
+
+for i in range(T):
+    tmp = f.readline()
+    tmp = tmp.split()
+    N[i] = int(tmp[0])
+    L[i] = int(tmp[1])
